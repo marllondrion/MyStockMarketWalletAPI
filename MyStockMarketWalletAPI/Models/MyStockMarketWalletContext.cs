b3a0f1074c2124a4cac6014a7a49536a086641a3
Marllon Drion <marllon@ares.com.au>
2021-05-12 00:12:32 +1000

    Add project files.

---
+++ MyStockMarketWalletAPI/Models/MyStockMarketWalletContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyStockMarketWalletAPI.Models
+{
+    public class MyStockMarketWalletContext : DbContext
+    {
+        public MyStockMarketWalletContext(DbContextOptions<MyStockMarketWalletContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+    }
+}
---
+++ MyStockMarketWalletAPI/Models/Stock.cs
@@ -0,0 +1,12 @@
+﻿namespace MyStockMarketWalletAPI.Models
+{
+    public class Stock
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string CNPJ { get; set; }
+        //public string Segment { get; set; }
+        //public bool xx { get; set; }
+    }
+}
